7e2144c86702103be89668553a7b3e5eede3d347
math-mcshane <math.mcshane@gmail.com>
2025-09-16 13:49:03 -0500

    Adding tests for file_validation

--- tests/testthat/test-get_parsed_tibble.R
+++ tests/testthat/test-get_parsed_tibble.R
@@ -1,3 +1,38 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
+test_that(
+  desc = "Validating file exists.", 
+  code = {
+    expect_no_error(
+      object = file_validation(
+        filename = example_script("generic_script")
+      )
+    )
+  }
+)
+
+test_that(
+  desc = "Ensuring error message exists.", 
+  code = {
+    expect_error(
+      object = file_validation(
+        filename = "this_file_does_not_exist.R"
+      )
+    )
+  }
+)
+
+test_that(
+  desc = "Verifying warning.", 
+  code = {
+    expect_warning(
+      object = file_validation(
+        filename = paste0(
+          system.file(package = "autogradeR"), 
+          "/../README.Rmd"
+        )
+      )
+    )
+  }
+)
+
+
+
