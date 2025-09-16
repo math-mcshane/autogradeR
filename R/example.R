702b0b916c26ceada22b66d5e8e8f15edbf2ca10
math-mcshane <math.mcshane@gmail.com>
2025-09-16 15:32:43 -0500

    Moving away from system.file to fs

--- R/example.R
+++ R/example.R
@@ -3,11 +3,12 @@
 #' @param name character string that is the name of the script
 #'
 #' @returns a character string which contains a full file location and name
+#' @importFrom fs path_package
 #' @export
 #'
 #' @examples
 #' example_script("generic_script")
 example_script = function(name) {
-  system.file(package = "autogradeR") |>
+  fs::path_package(package = "autogradeR") |>
     paste0("/examples/", name, ".R")
 }
